250f008d896475d40153cbab1024e16f02ad1e48
Ricardo <56237605+ricardo177m@users.noreply.github.com>
2023-05-09 22:18:19 +0000

    style: updated sign in button

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import FaceitIcon from "@/components/faceit";
 import { signIn, signOut } from "next-auth/react";
 
 export function SignOut() {
@@ -7,5 +8,13 @@ export function SignOut() {
 }
 
 export function SignInWithFaceit() {
-  return <button onClick={() => signIn("faceit")}>Sign in with FACEIT</button>;
+  return (
+    <button
+      onClick={() => signIn("faceit")}
+      className="inline-flex items-center gap-2 px-4 py-1 rounded-lg text-white bg-orange-600 hover:bg-orange-700 transition-colors"
+    >
+      <FaceitIcon className="w-6" />
+      Sign in with FACEIT
+    </button>
+  );
 }

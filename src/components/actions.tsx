9cc62edee0c8b24a6bf3ebc8d775d494c315f110
Ricardo <56237605+ricardo177m@users.noreply.github.com>
2023-06-12 14:42:32 +0000

    style: updated login & logout btns

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -2,19 +2,28 @@
 
 import FaceitIcon from "@/components/faceit";
 import { signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import { ImSpinner8 } from "react-icons/im";
+import { MdLogout } from "react-icons/md";
 
 export function SignOut() {
-  return <button onClick={() => signOut()}>Sign out</button>;
+  return <button onClick={() => signOut()}><MdLogout className="w-6" /></button>;
 }
 
 export function SignInWithFaceit() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <button
-      onClick={() => signIn("faceit")}
-      className="inline-flex items-center gap-2 px-4 py-1 rounded-lg text-white bg-orange-600 hover:bg-orange-700 transition-colors"
+      onClick={() => {
+        signIn("faceit");
+        setLoading(true);
+      }}
+      className="inline-flex items-center gap-2 px-4 py-1 rounded-lg text-white bg-orange-600 hover:bg-orange-700 disabled:bg-orange-700 transition-colors"
+      disabled={loading}
     >
-      <FaceitIcon className="w-6" />
-      Sign in with FACEIT
+      {loading ? <ImSpinner8 className="animate-spin h-6" /> : <FaceitIcon className="w-6" />}
+      Login with FACEIT
     </button>
   );
 }
--- src/components/ui/TopBar.tsx
+++ src/components/ui/TopBar.tsx
@@ -1,6 +1,7 @@
 import { SignInWithFaceit, SignOut } from "@/components/actions";
 import Logo from "@/components/ui/Logo";
 import { getServerSession } from "next-auth";
+import { ImageWithFallback } from "./ImageWithFallback";
 
 export default async function TopBar() {
   const session = await getServerSession();
@@ -11,10 +12,21 @@ export default async function TopBar() {
       <div>
         <span className="py-1 px-2 rounded-md bg-dark-600 text-sm">Beta</span>
       </div>
-      <div>
+      <div className="flex flex-row gap-4 items-center ml-auto">
         {session?.user ? (
           <>
-            <p>{session.user.name}</p> <SignOut />
+            <div className="inline-flex items-center gap-2">
+              <ImageWithFallback
+                src={session.user.image as string}
+                fallbackSrc="/assets/default-avatar.svg"
+                alt="Player avatar"
+                width="32"
+                height="32"
+                className="rounded-full border border-dark-700 aspect-square"
+              />
+              <p>{session.user.name}</p>
+            </div>
+            <SignOut />
           </>
         ) : (
           <SignInWithFaceit />

cf94ed1f8dfe57cd6341b6414033b7ca90783eef
Ricardo M <rmoreiradev@gmail.com>
2024-03-14 17:34:03 +0000

    chore(deps): updated deps

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,18 @@ import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["assets.faceit-cdn.net", "distribution.faceit-cdn.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.faceit-cdn.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "distribution.faceit-cdn.net",
+        pathname: "/**",
+      },
+    ],
     minimumCacheTTL: 60 * 60 * 12,
   },
   productionBrowserSourceMaps: true,

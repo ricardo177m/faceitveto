7aeda7d48c91eaaf0c32e279dc96a351a23ba1d4
Ricardo <rmoreiradev@gmail.com>
2024-11-01 16:36:05 +0000

    chore: cs2 cfg fetch

---
+++ src/lib/cs2cfg.ts
@@ -0,0 +1,11 @@
+import { SkillGroupMapping } from "@/types/skills";
+import { faceit } from "@/config/endpoints";
+
+export async function fetchSkillGroups(): Promise<SkillGroupMapping[]> {
+  const response = await fetch(faceit.skillGroups, {
+    cache: "force-cache",
+  });
+  const data = await response.json();
+  if (response.status === 200) return data.payload.level_mappings;
+  else throw new Error(data.errors[0].message);
+}

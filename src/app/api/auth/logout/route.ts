3281506139a81c8894c2db0f77c54c012ed658bf
Ricardo <56237605+ricardo177m@users.noreply.github.com>
2025-02-05 10:59:26 +0000

    fixed logout route cookie clear

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,8 +2,19 @@ import { NextResponse } from "next/server";
 
 import { config } from "@/config/config";
 
-export async function GET() {
+export async function GET(request: Request) {
   const response = new NextResponse(null, { status: 200 });
-  response.cookies.delete(config.cookies.token);
+  const host = request.headers.get("host");
+  if (!host) return NextResponse.json({ error: "🧐" }, { status: 400 });
+  response.cookies.set({
+    name: config.cookies.token,
+    value: "",
+    path: "/",
+    maxAge: -1,
+    sameSite: "lax",
+    secure: true,
+    httpOnly: true,
+    domain: host.includes("localhost") ? undefined : `.${host}`,
+  });
   return response;
 }
